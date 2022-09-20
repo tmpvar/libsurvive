b98da0ffc98be6336ccc3a79005df1b2ea7110d3
Elijah Insua <tmpvar@gmail.com>
2022-09-20 02:59:02 -0700

    move remote hid app into a c++ application using flatbuffers for the protocol

--- remote-hid/remote-hid-libsurvive.cpp
+++ remote-hid/remote-hid-libsurvive.cpp
@@ -9,7 +9,7 @@
 #include <survive_api.h>
 
 #include <xxhash.h>
-
+#include "remote-hid_generated.h"
 typedef unsigned long long u64;
 typedef long long i64;
 typedef unsigned int u32;
@@ -43,44 +43,58 @@ static void log_fn(SurviveSimpleContext *actx, SurviveLogLevel logLevel, const c
 
 #define HOTCART_PORT 49222
 
-#pragma pack(push, 1)
-struct FullState {
-	struct {
-		f32 x;
-		f32 y;
-		f32 z;
-	} pos;
-	struct {
-		f32 x;
-		f32 y;
-		f32 z;
-		f32 w;
-	} rot;
-	u16 buttonsCount;
-	u16 analogsCount;
-	u16 joysticksCount;
-
-	u32 buttons;
-	// f32 analogs[HOTCART_REMOTE_HID_ANALOGS_MAX];
-	// struct { f32 x, f32 y } joysticks[HOTCART_REMOTE_HID_JOYSTICKS_MAX];
+struct SocketState {
+	SOCKET sock;
+	struct sockaddr_in addr;
 };
 
-struct LogEntry {
-	u16 length;
-	char msg[512];
-};
+static void emit(SocketState *socketState, SurviveSimpleContext *ctx, const SurviveSimpleObject *object) {
+	
+	flatbuffers::FlatBufferBuilder builder(1024);
 
-struct RemoteHIDPacket {
-	u16 type;		// RemoteHIDPacketType
-	u16 deviceType; // RemoteHIDDeviceType
-	u64 deviceID;	// hashed value
+	SurvivePose pose;
+	survive_simple_object_get_latest_pose(object, &pose);
 
-	union {
-		struct FullState fullState;
-		struct LogEntry logEntry;
+	// swizzle to y+ up
+	RemoteHID::v3 pos(pose.Pos[0], pose.Pos[2], pose.Pos[1]);
+
+	// Swizzle wxyz (up=z+) -> xzyw (up=y+)
+	RemoteHID::quat rot(pose.Rot[1], pose.Rot[3], pose.Rot[2], -pose.Rot[0]);
+
+	u32 buttonMasks[1] = {u32(survive_simple_object_get_button_mask(object))};
+
+	f32 analogs[1] = {
+		f32(survive_simple_object_get_input_axis(object, SurviveAxis::SURVIVE_AXIS_TRIGGER)),
 	};
-};
-#pragma pack(pop)
+
+	RemoteHID::v2 joysticks[1] = {
+		RemoteHID::v2(survive_simple_object_get_input_axis(object, SURVIVE_AXIS_TRACKPAD_X),
+					  survive_simple_object_get_input_axis(object, SURVIVE_AXIS_TRACKPAD_Y))};
+
+	auto fullState = RemoteHID::CreatePayloadFullState(builder, &pos, &rot, builder.CreateVector(buttonMasks, 1),
+													   builder.CreateVector(analogs, 1),
+													   builder.CreateVectorOfStructs(joysticks, 1));
+
+	const char *serial = survive_simple_serial_number(object);
+	const u64 deviceID = XXH64(serial, strlen(serial), 0);
+
+	auto packet = RemoteHID::CreatePacket(builder, RemoteHID::DeviceType::DeviceType_SixDOF, deviceID,
+										  RemoteHID::Payloads_PayloadFullState, fullState.Union());
+
+	builder.Finish(packet);
+	// printf("%s %s (%7.3f): %f %f %f %f %f %f %f\n", survive_simple_object_name(pose_event->object),
+	// 	   survive_simple_serial_number(pose_event->object), timecode, pose.Pos[0], pose.Pos[1], pose.Pos[2],
+	// 	   pose.Rot[0], pose.Rot[1], pose.Rot[2], pose.Rot[3]);
+
+	int sent = sendto(
+	  socketState->sock,
+	  (char *)builder.GetBufferPointer(),
+	  builder.GetSize(),
+	  0,
+	  (struct sockaddr *)&socketState->addr,
+	  sizeof(socketState->addr)
+	);
+}
 
 int main(int argc, char **argv) {
 #ifdef __linux__
@@ -96,20 +110,21 @@ int main(int argc, char **argv) {
 		exit(EXIT_FAILURE);
 	}
 
-	SOCKET sock = socket(AF_INET, SOCK_DGRAM, 0);
-	if (sock == INVALID_SOCKET) {
+	SocketState socketState;
+
+	socketState.sock = socket(AF_INET, SOCK_DGRAM, 0);
+	if (socketState.sock == INVALID_SOCKET) {
 		printf("Could not create socket : %d", WSAGetLastError());
 		return 1;
 	}
 
-	struct sockaddr_in addr;
-	addr.sin_family = AF_INET;
-	addr.sin_addr.s_addr = INADDR_ANY;
-	addr.sin_port = htons(HOTCART_PORT);
-	addr.sin_addr.s_addr = inet_addr("192.168.0.255");
+	socketState.addr.sin_family = AF_INET;
+	socketState.addr.sin_addr.s_addr = INADDR_ANY;
+	socketState.addr.sin_port = htons(HOTCART_PORT);
+	socketState.addr.sin_addr.s_addr = inet_addr("192.168.0.255");
 
 	char broadcast = 1;
-	setsockopt(sock, SOL_SOCKET, SO_BROADCAST, &broadcast, sizeof(broadcast));
+	setsockopt(socketState.sock, SOL_SOCKET, SO_BROADCAST, &broadcast, sizeof(broadcast));
 #endif
 
 	SurviveSimpleContext *actx = survive_simple_init_with_logger(argc, argv, log_fn);
@@ -124,41 +139,23 @@ int main(int argc, char **argv) {
 		printf("Found '%s'\n", survive_simple_object_name(it));
 	}
 
-	struct SurviveSimpleEvent event = {0};
+	struct SurviveSimpleEvent event = {};
 
 	u32 buttonStates = 0;
 	while (keepRunning && survive_simple_wait_for_event(actx, &event) != SurviveSimpleEventType_Shutdown) {
 		switch (event.event_type) {
 		case SurviveSimpleEventType_PoseUpdateEvent: {
 			const struct SurviveSimplePoseUpdatedEvent *pose_event = survive_simple_get_pose_updated_event(&event);
-			SurvivePose pose = pose_event->pose;
-			FLT timecode = pose_event->time;
-			// printf("%s %s (%7.3f): %f %f %f %f %f %f %f\n", survive_simple_object_name(pose_event->object),
-			// 	   survive_simple_serial_number(pose_event->object), timecode, pose.Pos[0], pose.Pos[1], pose.Pos[2],
-			// 	   pose.Rot[0], pose.Rot[1], pose.Rot[2], pose.Rot[3]);
-
-			struct RemoteHIDPacket packet;
-			memset(&packet, 0, sizeof(packet));
-			packet.type = 1;	   // FullState
-			packet.deviceType = 2; // SixDOF
-			char *serial = survive_simple_serial_number(pose_event->object);
-			packet.deviceID = XXH64(serial, strlen(serial), 0);
-			// Swizzle to y-up
-			packet.fullState.pos.x = pose.Pos[0];
-			packet.fullState.pos.y = pose.Pos[2];
-			packet.fullState.pos.z = pose.Pos[1];
-
-			// Swizzle wxyz (up=z+) -> xzyw (up=y+)
-			packet.fullState.rot.w = -pose.Rot[0];
-			packet.fullState.rot.x = pose.Rot[1];
-			packet.fullState.rot.y = pose.Rot[3];
-			packet.fullState.rot.z = pose.Rot[2];
-
-			packet.fullState.buttonsCount = 1;
-			packet.fullState.buttons = survive_simple_object_get_button_mask(pose_event->object);
-			int sent = sendto(sock, (char *)&packet, sizeof(packet), 0, (struct sockaddr *)&addr, sizeof(addr));
+			emit(&socketState, actx, pose_event->object);
 			break;
 		}
+		case SurviveSimpleEventType_ButtonEvent: {
+			const struct SurviveSimpleButtonEvent *button_event = survive_simple_get_button_event(&event);
+			emit(&socketState, actx, button_event->object);
+			break;
+		}
+
+		/*
 		case SurviveSimpleEventType_ButtonEvent: {
 			const struct SurviveSimpleButtonEvent *button_event = survive_simple_get_button_event(&event);
 			SurviveObjectSubtype subtype = survive_simple_object_get_subtype(button_event->object);
@@ -182,7 +179,8 @@ int main(int argc, char **argv) {
 			}
 			printf("\n");
 			break;
-		}
+		}*/
+
 		case SurviveSimpleEventType_ConfigEvent: {
 			const struct SurviveSimpleConfigEvent *cfg_event = survive_simple_get_config_event(&event);
 			printf("(%f) %s received configuration of length %u type %d-%d\n", cfg_event->time,
